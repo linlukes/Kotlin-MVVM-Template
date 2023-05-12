0edbb2b54534909ea0fcf377cac790ef449dad7a
lin <linpengtian2@gmail.com>
2023-05-12 14:02:21 +0100

    fixed an issue.

--- app/src/main/java/com/harmoush/photoweather/data/source/repository/SampleRepository.kt
+++ app/src/main/java/com/harmoush/photoweather/data/source/repository/SampleRepository.kt
@@ -1,9 +1,11 @@
 package com.harmoush.photoweather.data.source.repository
 
+import androidx.lifecycle.LiveData
 import com.harmoush.photoweather.data.model.WeatherInfo
 import com.harmoush.photoweather.data.source.local.DatabaseResource
 import com.harmoush.photoweather.data.source.local.entity.SampleEntity
 import com.harmoush.photoweather.data.source.remote.NetworkBoundResource
+import com.voctag.android.model.Resource
 import kotlinx.coroutines.CoroutineScope
 
 /*
@@ -13,5 +15,5 @@ Created by Harmoush on 2020-11-06
 interface SampleRepository {
 
     fun getSampleEntities(scope: CoroutineScope): NetworkBoundResource<List<SampleEntity>, WeatherInfo>
-    fun insertEntity(scope: CoroutineScope, entity: SampleEntity): DatabaseResource<Int>
+    fun insertEntity(scope: CoroutineScope, entity: SampleEntity): LiveData<Resource<Int>>
 }
